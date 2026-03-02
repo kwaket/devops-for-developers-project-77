080f4d4ac69a5c6e37603b2a0b6d891a9dd48a31
savva <kwaket@outlook.com>
2026-03-02 20:59:14 +0300

    refactor: extract non-sensitive tf backend vars from vault

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,14 @@
 terraform {
   backend "s3" {
-    endpoint                    = "https://storage.yandexcloud.net"
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
     region                      = "ru-central1"
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_requesting_account_id  = true
     skip_s3_checksum            = true
+    bucket                      = "yc-savangard-state"
+    key                         = "savangard-remote-state"
   }
 }
